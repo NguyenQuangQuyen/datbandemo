413d954ba1b700315c0a9a0f42489ff9e726a127
NguyenQuangQuyen <quyenquy053#gmail.com>
2024-11-25 17:18:13 +0700

    test

---
+++ Web_Nha_Hang/Web_Nha_Hang/Areas/AdminArea/Controllers/BanController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using PagedList;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Web_Nha_Hang.Models;
+
+namespace Web_Nha_Hang.Areas.AdminArea.Controllers
+{
+    public class BanController : Controller
+    {
+        private DBConnectNhaHang db = new DBConnectNhaHang();
+
+        // GET: AdminArea/Ban
+        public ActionResult Index(int? page)
+        {
+            int pageSize = 5; // Số lượng sản phẩm trên mỗi trang
+            int pageNumber = (page ?? 1); // Số trang hiện tại, mặc định là 1 nếu không có giá trị
+            var model = db.Bans.OrderBy(b => b.maban).ToPagedList(pageNumber, pageSize);
+            return View(model);
+        }
+
+        // GET: AdminArea/Ban/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ban ban = db.Bans.Find(id);
+            if (ban == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ban);
+        }
+
+        // GET: AdminArea/Ban/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminArea/Ban/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "maban,tenban,trangthai,soluong")] Ban ban)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bans.Add(ban);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(ban);
+        }
+
+        // GET: AdminArea/Ban/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ban ban = db.Bans.Find(id);
+            if (ban == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ban);
+        }
+
+        // POST: AdminArea/Ban/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "maban,tenban,trangthai,soluong")] Ban ban)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ban).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(ban);
+        }
+
+        // GET: AdminArea/Ban/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ban ban = db.Bans.Find(id);
+            if (ban == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ban);
+        }
+
+        // POST: AdminArea/Ban/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ban ban = db.Bans.Find(id);
+            db.Bans.Remove(ban);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Web_Nha_Hang/Web_Nha_Hang/Controllers/NhaHangController.cs
@@ -0,0 +1,83 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using Web_Nha_Hang.Models;
+
+namespace Web_Nha_Hang.Controllers
+{
+    public class NhaHangController : Controller
+    {
+        // GET: NhaHang
+        private DBConnectNhaHang db = new DBConnectNhaHang();
+        public ActionResult HomePage()
+        {
+            ViewBag.Username = Session["user"] as string;
+            var listMenu = new DBConnectNhaHang().Monans.ToList();
+            return View(listMenu);
+        }
+        public ActionResult Introduce()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult SpecialOffer()
+        {
+            return View();
+        }
+        public ActionResult Menu(int? page)
+        {
+            int pageSize = 6;
+            int pageNumber = (page ?? 1);
+            var listMenu = new DBConnectNhaHang().Monans.Include("DanhMuc").ToList().ToPagedList(pageNumber, pageSize);
+            var listDanhMuc = new DBConnectNhaHang().Danhmucs.ToList();
+            ViewBag.ListDanhMuc = listDanhMuc;
+            return View(listMenu);
+        }
+        public ActionResult MenuDetail(int id)
+        {
+            var product = new DBConnectNhaHang().Monans.FirstOrDefault(p => p.mamon == id);
+
+            if (product == null)
+            {
+                return HttpNotFound(); // Hoặc chuyển hướng đến trang 404
+            }
+
+            return View(product);
+        }
+        public ActionResult Reserve_a_table([Bind(Include = "madb,maban,hoten,sdt,soluongkh,ngaydat,ghichu")] Datban datban)
+        {
+
+            if (Session["user"] == null)
+            {
+                /*return RedirectToAction("Index", "Dashbroad");*/
+                return RedirectToAction("Login", "Default", new { area = "AdminArea" });
+            }
+            else
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Datbans.Add(datban);
+                    db.SaveChanges();
+                    return RedirectToAction("Reserve_a_table");
+                }
+
+                ModelState.Clear();
+                TempData["SuccessMessage"] = "Đặt bàn thành công!";
+                ViewBag.maban = new SelectList(db.Bans, "maban", "tenban", datban.maban);
+                return View(datban);
+            }
+        }
+
+    }
+}
